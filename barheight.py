36fe86e32ae91cc296b56e044149e4415ceafe74
themanofcat <themanofcat@gmail.com>
2020-06-08 13:16:02 -0400

    Turned into a class

--- barheight.py
+++ barheight.py
@@ -16,42 +16,60 @@ def bar_height(frame, blocks, points_per_bar):
 
     return np.array(heights)
 
-# given an audio file and samples_per_frame (which is the sample rate divided by
-# the frame rate), decomposes the audio and returns a 2D numpy array where 
-# axis 0 is frame and axis 1 is the height of the bar in that frame
-def decompose(samples_per_frame, blocks, audio):
-    print("converting to mono")
-    if audio.shape[1] > 1:
-        # convert stereo to mono via simply adding the channels
-        audio = np.sum(audio, axis=1)
-    
-    # now 'audio' represents an array of audio frames, where each array
-    # corresponds to a frame in the video
-    print(f"converted. resizing {audio.shape[0]}")
-    audio.resize(samples_per_frame, audio.shape[0]//samples_per_frame-1)
-    
-    # transformed applies a real-values fast fourier transform on each frame
-    # a frame is a sectiond of samples in the wav file, as framerate goes up
-    # the accuracy decreases, while the framerate of the video increases
-    # 60 fps is probably a good compromise
-    print(f"resized. transforming {audio.shape}->{audio.shape[0]*audio.shape[1]}")
-    transformed = vfft(a=audio)
 
-    # the output of the rfft is complex, we want the magnitudes of these numbers
-    # to get the actual magnitude of that frequency (which will be the height of the bar)
-    print(f"transformed, taking magnitudes {transformed.shape}")
-    transformed = magnitude(transformed)
-    print("magnitudes taken, calculating bar heights")    
-    
-    points_per_bar = (transformed.shape[1]//blocks) // 16
-    #                 ^samples per frame    ^ per bar  ^ we only *really* want the info 
-    #                                                    stored in the lower part
+class VideoController:
+    def __init__(self, vheight, vwidth, framerate, blocks, audiofile):
+        """ vheight/width are the dimensions of the video
+            framerate is the framerate
+            blocks is the amount of bars to be displayed in each frame
+            audiofile is the scipy.io.wavfile (after conversion) that's being used """
+        
+        self.vheight = vheight
+        self.vwidth = vwidth
+        self.framerate = framerate
+        self.blocks = blocks
 
-    get_heights = np.vectorize(bar_height, \
-            signature='(m),()->(k)')
-    video_bar_heights = get_heights(transformed, blocks, points_per_bar)
-    
-    print(f"bar heights calculated {video_bar_heights.shape}")
+        self.samples_per_frame = audiofile[0]//framerate
+        self.audio = audiofile[1]
 
-    return video_bar_heights 
+
+    def decompose(self):
+        """ given an audio file and samples_per_frame (which is the sample rate divided by
+            the frame rate), decomposes the audio and returns a 2D numpy array where 
+            along axis 0 are the frames and along axis 1 are the heights of the bars in that frame """
+
+        print("converting to mono")
+        if len(self.audio.shape) > 1 and self.audio.shape[1] > 1:
+            # convert stereo to mono via simply adding the channels
+            self.audio = np.sum(self.audio, axis=1)
+        
+        # now 'audio' represents an array of audio frames, where each array
+        # corresponds to a frame in the video
+        print(f"converted. resizing {self.audio.shape[0]}")
+        self.audio.resize(self.samples_per_frame, self.audio.shape[0]//self.samples_per_frame-1)
+        
+        # transformed applies a real-values fast fourier transform on each frame
+        # a frame is a sectiond of samples in the wav file, as framerate goes up
+        # the accuracy decreases, while the framerate of the video increases
+        # 60 fps is probably a good compromise
+        print(f"resized. transforming {self.audio.shape}->{self.audio.shape[0]*self.audio.shape[1]}")
+        transformed = vfft(self.audio)
+
+        # the output of the rfft is complex, we want the magnitudes of these numbers
+        # to get the actual magnitude of that frequency (which will be the height of the bar)
+        print(f"transformed, taking magnitudes {transformed.shape}")
+        transformed = magnitude(transformed)
+        print("magnitudes taken, calculating bar heights")    
+        
+        points_per_bar = (transformed.shape[1]//self.blocks) // 16
+        #                 ^samples per frame    ^ per bar  ^ we only *really* want the info 
+        #                                                    stored in the lower part
+
+        get_heights = np.vectorize(bar_height, \
+                signature='(m),(),()->(k)')
+        video_bar_heights = get_heights(transformed, self.blocks, points_per_bar)
+        
+        print(f"bar heights calculated {video_bar_heights.shape}")
+
+        return video_bar_heights 
 
